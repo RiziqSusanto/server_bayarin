8a881c9285f3defa865f19efe3c364791c128363
RiziqSusanto <65065233+RiziqSusanto@users.noreply.github.com>
2020-08-26 22:13:34 +0700

    Add Cors

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const methodOverride = require('method-override');
-const session = require('express-session');
-const flash = require('connect-flash');
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const cors = require("cors");
 // Import mongoose
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/db_sppBaru', {
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/db_sppBaru", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -18,21 +19,24 @@ mongoose.connect('mongodb://localhost:27017/db_sppBaru', {
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 // router admin
-const adminRouter = require('./routes/admin')
-const apiRouter = require('./routes/api')
+const adminRouter = require("./routes/admin");
+const apiRouter = require("./routes/api");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(methodOverride('_method'))
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+app.use(cors());
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
